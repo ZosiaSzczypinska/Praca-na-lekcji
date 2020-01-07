72488c05d4bf114c5669deb23e9ddcc16f2548d5
ZosiaSzczypinska <58827987+ZosiaSzczypinska@users.noreply.github.com>
2020-01-07 15:16:05 +0100

    Add files via upload

---
+++ Praca na lekcji2.cpp
@@ -0,0 +1,31 @@
+/// Zamiana liczby dziesietnej na dwojkowa
+/// autor: Zofia Szczypiñska
+/// Data: 2020-01-07
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int dec;
+    string bin = "";
+
+    cin >> dec;
+
+    while(dec>0)
+    {
+        if (dec%2==0)
+            bin +="0";
+        else
+            bin += "1";
+        dec /= 2;
+    }
+    /// algorithm
+    reverse(bin.begin(), bin.end());
+    cout << bin;
+
+
+    return 0;
+}
