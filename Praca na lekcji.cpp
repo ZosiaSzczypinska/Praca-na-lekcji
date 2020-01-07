ec9b6ba21afe75ba1dad59cd548ff0f4d91339c2
ZosiaSzczypinska <58827987+ZosiaSzczypinska@users.noreply.github.com>
2020-01-07 15:01:30 +0100

    Add files via upload

---
+++ Praca na lekcji.cpp
@@ -0,0 +1,24 @@
+/// suma cyfr liczby ca³kowitej
+/// autor: Zofia Szczypiñska
+/// Data: 2020-01-07
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number, r;
+    int sum = 0;
+    cin >> number;
+
+    while(number>0)
+    {
+        r = number % 10;
+        sum += r;
+        number /= 10;
+    }
+    cout << sum;
+    return 0;
+
+}
